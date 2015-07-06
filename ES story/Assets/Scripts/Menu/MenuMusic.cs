c708001bf49d1b7bc09d865a7c9f2b6d3bf97801
Nikita Ivanov <nikitaivanov95@gmail.com>
2015-07-06 23:49:47 +0500

    Version 0.62
    Добавлен 6 день, добавлена музыка в главное меню

---
+++ ES story/Assets/Scripts/Menu/MenuMusic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.audio.mute = !Settings.MusicOn;
+	}
+}
